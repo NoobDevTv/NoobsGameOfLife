110337388a441d805dec9a90bd9adb37e818d053
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2019-03-10 19:56:36 +0100

    Add Elements

---
+++ NoobsGameOfLife.Core/Element.cs
@@ -0,0 +1,16 @@
+﻿namespace NoobsGameOfLife.Core
+{
+    public struct Element
+    {
+        public string Name { get; private set; }
+
+        private Element(string name)
+        {
+            Name = name;
+        }
+
+        public static Element Carbon => new Element("carbon");
+        public static Element Oxygen => new Element("oxygen");
+        public static Element Hydrogen => new Element("hydrogen");
+    }
+}
---
+++ NoobsGameOfLife.Core/NutrientTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoobsGameOfLife.Core
+{
+    public class NutrientTypes
+    {
+        Dictionary<Element, byte> Elements;
+        public NutrientTypes()
+        {
+            Elements = new Dictionary<Element, byte> {};
+        }
+    }
+}
